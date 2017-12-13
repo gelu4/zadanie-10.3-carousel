908d72750ec93269eb56a5b58642b2876d40f1d2
Grzegorz Koza <kozagrzegorz@interia.pl>
2017-12-13 20:27:31 +0100

    Add change slide components

--- script.js
+++ script.js
@@ -3,17 +3,18 @@ $(function(){
 	
 var carouselList = $("#carousel ul");
 
-
-	setInterval(changeSlide(){
-			carouselList.animate({'margin-left':-400}, 500);
-
-
+setInterval(function changeSlide(){
+	carouselList.animate({'margin-left':-400}, 1000, moveFirstSlide);
 			
 }, 3000);
 
+	function moveFirstSlide(){
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	
+	}
 
-
-
-
-
+	
 });
